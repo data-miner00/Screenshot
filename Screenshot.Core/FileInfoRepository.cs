fe1f8363009ab6c692bb5b8d267abbab9e2c8fc1
data-miner00 <data-miner00@users.noreply.github.com>
2024-07-09 21:33:53 +0800

    feat: Add file info repository

---
+++ Screenshot.Core/FileInfoRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Screenshot.Core;
+
+using System.IO;
+
+public sealed class FileInfoRepository : IFileInfoRepository
+{
+    public FileInfo[] GetFileInfo(string folderPath)
+    {
+        var directoryInfo = new DirectoryInfo(folderPath);
+
+        var files = directoryInfo.GetFiles();
+
+        return files;
+    }
+}
---
+++ Screenshot.Core/IFileInfoRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Screenshot.Core;
+
+using System.IO;
+
+public interface IFileInfoRepository
+{
+    public FileInfo[] GetFileInfo(string folderPath);
+}
